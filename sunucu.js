c7350b9dfe9e039c8eddc9ec3e8902920f9f7354
berkay3229 <berkayyaman32@gmail.com>
2020-04-10 12:37:51 +0300

    python dosyaları eklendi

--- sunucu.js
+++ sunucu.js
@@ -1,9 +1,15 @@
 var http = require('http');
 var fs = require('fs');
 http.createServer(function (req, res) {
-  fs.readFile('demofile1.html';, function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
+  fs.readFile('html/denme.html', function(err, data) {
+    //res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     res.end();
   });
-}).listen(8080);
+
+  
+}
+
+
+
+).listen(8080);
